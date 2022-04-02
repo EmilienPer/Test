37ef23456e13ac3d67f16278bd5682a2f3664fc2
STIB-MIVB\sirtolis <sebastien.sirtoli@stib.brussels>
2022-04-02 23:16:04 +0200

    remove git sha

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
 import os
 from flask import Flask, render_template
 app = Flask(__name__)
-import git
 
 
 @app.route('/')
 def hello_world():
-   sha: "N/A"
-   try:
-      repo = git.Repo(search_parent_directories=True)
-      sha = repo.head.object.hexsha
-   except:
-      pass
    return render_template('index.html', sha=sha)
 
 
