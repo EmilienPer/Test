930bb87e5bfe88f118695b2b9b8727713cedd338
STIB-MIVB\sirtolis <sebastien.sirtoli@stib.brussels>
2022-04-27 07:42:07 +0200

    Add prometheus monitoring

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
 import os
 from flask import Flask, render_template
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Summary
 from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 csrf = CSRFProtect(app)
-   
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0')
+duration = Summary('duration_compute_seconds', 'Time spent in the compute() function')
+
+@duration.time()
 @app.route('/')
 def hello_world():
    return render_template('index.html')
@@ -11,4 +17,4 @@ def hello_world():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
