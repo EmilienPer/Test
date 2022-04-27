65ed2b7a2d8bd08b08c8208601f26f8ff57ba49f
STIB-MIVB\sirtolis <sebastien.sirtoli@stib.brussels>
2022-04-27 19:58:16 +0200

    Move to blue

--- call_generator.sh
+++ call_generator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for i in {1..10}; do
+while true
+do
     for i in {1..$(( $RANDOM % 10+ 1 ))}; do
         curl --silent "https://devops-talk.herokuapp.com/" -o /dev/null
         echo $i
